f6c79fdfc396678aa11c589a1e94c43e5b04e51b
Shanley Demsich <shanleyd@seas.upenn.edu>
2022-12-04 17:04:03 -0500

    Created Day 4 Part 1

---
+++ Day 4/MainFour.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Map;
+
+public class MainFour {
+
+    public static void main(String[] args) throws Exception {
+        // Day 4: File path is passed as parameter
+        File file = new File("sectionAssignments.txt");
+
+        // Day 4: Create a buffered reader
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+//        // Day 4 Part 1: Find how many cleaning elf assignment pairs fully contain one of the partner's assinments
+//        int assignmentContainment = findAssignmentContainment(br);
+//        System.out.println(assignmentContainment);
+
+        // Day 4 Part 2:
+//        System.out.println();
+    }
+}
