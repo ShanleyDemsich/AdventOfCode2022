7e2073089b035a7b5020c884a463395b433b215a
Shanley Demsich <shanleyd@seas.upenn.edu>
2023-02-22 16:08:30 -0500

    Added Day 6

---
+++ Day 6/MainSix.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class MainSix {
+
+    public static void main(String[] args) throws Exception {
+        // Day 6: File path is passed as parameter
+        File file = new File("datastream.txt");
+
+        // Day 6: Create a buffered reader
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        // Day 6 Part 1:
+//        System.out.println();
+
+        // Day 6 Part 2:
+//        System.out.println();
+
+    }
+}
