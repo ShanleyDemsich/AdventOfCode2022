510c3900965135412a2a3453f38e3476266e464d
Shanley Demsich <shanleyd@seas.upenn.edu>
2023-02-23 17:56:48 -0500

    implemented createAlphabetDictionary()

--- Day 6/MainSix.java
+++ Day 6/MainSix.java
@@ -6,6 +6,56 @@ import java.util.*;
 
 public class MainSix {
 
+    public static int findDataMarker(BufferedReader br) throws IOException {
+        Dictionary<Character, Integer> alphabet = createAlphabetDictionary();
+
+        String str;
+
+        int count = 0;
+        int c = 0;
+        // continue while there are still characters to be read
+        while ((c = br.read()) != -1) {
+            if (alphabet.get(c) == 0){
+                
+            }
+        }
+
+        return 4;
+    }
+
+    public static Dictionary<Character, Integer> createAlphabetDictionary () {
+        Dictionary<Character, Integer> alphabet = new Hashtable<>();
+
+        alphabet.put('a', 0);
+        alphabet.put('b', 0);
+        alphabet.put('c', 0);
+        alphabet.put('d', 0);
+        alphabet.put('e', 0);
+        alphabet.put('f', 0);
+        alphabet.put('g', 0);
+        alphabet.put('h', 0);
+        alphabet.put('i', 0);
+        alphabet.put('j', 0);
+        alphabet.put('k', 0);
+        alphabet.put('l', 0);
+        alphabet.put('m', 0);
+        alphabet.put('n', 0);
+        alphabet.put('o', 0);
+        alphabet.put('p', 0);
+        alphabet.put('q', 0);
+        alphabet.put('r', 0);
+        alphabet.put('s', 0);
+        alphabet.put('t', 0);
+        alphabet.put('u', 0);
+        alphabet.put('v', 0);
+        alphabet.put('w', 0);
+        alphabet.put('x', 0);
+        alphabet.put('y', 0);
+        alphabet.put('z', 0);
+
+        return alphabet;
+    }
+
     public static void main(String[] args) throws Exception {
         // Day 6: File path is passed as parameter
         File file = new File("datastream.txt");
@@ -14,7 +64,7 @@ public class MainSix {
         BufferedReader br = new BufferedReader(new FileReader(file));
 
         // Day 6 Part 1:
-//        System.out.println();
+        System.out.println(br);
 
         // Day 6 Part 2:
 //        System.out.println();
