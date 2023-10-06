452b992301075ef10c8d53209e99c53262e57533
Shanley Demsich <shanleyd@seas.upenn.edu>
2023-10-06 13:51:32 -0400

    Implemented is_tree_visible

--- Day 8/MainEight.py
+++ Day 8/MainEight.py
@@ -1,4 +1,6 @@
 def create_tree_grid():
+    """Turns the .txt tree grid file into a workable dictionary"""
+
     tree_row = []
     tree_grid_dict = {}
     row = 1
@@ -14,6 +16,29 @@ def create_tree_grid():
             tree_row = []  # reset the tree row for the next row of trees
             row += 1  # increment the row counter
     return tree_grid_dict
+
+
+def get_tree_grid():
+    return create_tree_grid()
+
+
+def count_visible_trees():
+    """Counts the total number of visible trees in the tree grid.
+    A visible tree is any that can be seen from above, below, to the left
+    or to the right of the tree. """
+
+    tree_grid_dict = get_tree_grid()
+    above_row = tree_grid_dict[1]
+    current_row = tree_grid_dict[2]
+    below_row = tree_grid_dict[3]
+
+
+def is_tree_visible(current, above, below, left, right):
+    """Verifies if the current tree is taller than, able to be seen, the
+    trees left, right, above, or below"""
+
+    return current > above and current > below \
+           and current > left and current > right
 
 
 # Day 8, part 1
