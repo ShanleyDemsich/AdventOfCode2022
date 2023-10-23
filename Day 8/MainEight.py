25627a45c34b72ed7401e1925c09408e8bb38ac8
Shanley Demsich <shanleyd@seas.upenn.edu>
2023-10-23 13:55:55 -0400

    passing create_tree_grid_mask tests

--- Day 8/MainEight.py
+++ Day 8/MainEight.py
@@ -28,39 +28,51 @@ def create_tree_grid_mask(tree_grid):
     """Create masks of the visible trees from looking left, right, up
     or down a matrix of tree heights"""
     tree_grid = np.array(tree_grid)
-    left_to_right_tree_mask = []
-    right_to_left_tree_mask = []
-    top_to_bottom_tree_mask = []
-    bottom_to_top_tree_mask = []
+    # left_to_right_tree_mask = []
+    # right_to_left_tree_mask = []
+    # top_to_bottom_tree_mask = []
+    # bottom_to_top_tree_mask = []
+
+    mask_1 = []
+    mask_2 = []
+    mask_3 = []
+    mask_4 = []
 
     for row in tree_grid:
         # Label visible trees from left to right
-        left_to_right_tree_mask.append(visible_row_trees(row))
+        # left_to_right_tree_mask.append(visible_row_trees(row))
+        mask_1.append(visible_row_trees(row))
+
 
     # rotate the tree grid 90 degrees clockwise
     tree_grid = np.rot90(tree_grid)
 
     for row in tree_grid:
         # Label visible trees from bottom to top
-        bottom_to_top_tree_mask.append(visible_row_trees(row))
+        # bottom_to_top_tree_mask.append(visible_row_trees(row))
+        mask_2.append(visible_row_trees(row))
+
 
     # rotate the tree grid 90 degrees clockwise
     tree_grid = np.rot90(tree_grid)
 
     for row in tree_grid:
         # Label visible trees from right to left
-        right_to_left_tree_mask.append(visible_row_trees(row))
+        # right_to_left_tree_mask.append(visible_row_trees(row))
+        mask_3.append(visible_row_trees(row))
+
 
     # rotate the tree grid 90 degrees clockwise
     tree_grid = np.rot90(tree_grid)
 
     for row in tree_grid:
         # Label visible trees from top to bottom
-        top_to_bottom_tree_mask.append(visible_row_trees(row))
+        # top_to_bottom_tree_mask.append(visible_row_trees(row))
+        mask_4.append(visible_row_trees(row))
 
-    return left_to_right_tree_mask, right_to_left_tree_mask, \
-           top_to_bottom_tree_mask, bottom_to_top_tree_mask
 
+    # return left_to_right_tree_mask, bottom_to_top_tree_mask, right_to_left_tree_mask, top_to_bottom_tree_mask
+    return mask_1, mask_2, mask_3, mask_4
 
 def combine_tree_grid_masks(left_right_mask, right_left_mask, top_bottom_mask,
                             bottom_top_mask):
--- Day 8/test.py
+++ Day 8/test.py
@@ -50,93 +50,52 @@ class MyTestCase(unittest.TestCase):
 
         actual_1, actual_2, actual_3, actual_4 = create_tree_grid_mask(
             tree_grid)
+
         # expected_1 = [[0, 1, 2, 8],
         #               [1, 1, 3, 2],
         #               [5, 1, 4, 6],
         #               [2, 2, 0, 0]]
 
-        # rotated 90 degrees clockwise
-        # expected_2 = [[2, 5, 1, 0],
-        #               [2, 1, 1, 1],
-        #               [0, 4, 3, 2],
-        #               [0, 6, 2, 8]]
+        # rotated 90 degrees counter-clockwise
+        # expected_2 = [[8, 2, 6, 0],
+        #               [2, 3, 4, 0],
+        #               [1, 1, 1, 2],
+        #               [0, 1, 5, 2]]
 
-        # rotated 90 degrees clockwise
+        # rotated 90 degrees counter-clockwise
         # expected_3 = [[0, 0, 2, 2],
         #               [6, 4, 1, 5],
         #               [2, 3, 1, 1],
         #               [8, 2, 1, 0]]
 
-        # rotated 90 degrees clockwise
-        # expected_4 = [[8, 2, 6, 0],
-        #               [2, 3, 4, 0],
-        #               [1, 1, 1, 2],
-        #               [0, 1, 5, 2]]
-
-        ##############################################################
-
-        # expected_1 = [[0, 1, 2, 8],
-        #               [1, 1, 3, 2],
-        #               [5, 1, 4, 6],
-        #               [2, 2, 0, 0]]
-
         # rotated 90 degrees counter-clockwise
-        # expected_2 = [[8, 2, 1, 0],
-        #               [2, 3, 1, 1],
-        #               [6, 4, 1, 5],
-        #               [0, 0, 2, 2]]
+        # expected_4 = [[2, 5, 1, 0],
+        #               [2, 1, 1, 1],
+        #               [0, 4, 3, 2],
+        #               [0, 6, 2, 8]]
 
-        # rotated 90 degrees counter-clockwise
-        # expected_3 = [[0, 1, 2, 8],
-        #               [1, 1, 3, 2],
-        #               [5, 1, 4, 6],
-        #               [2, 2, 0, 0]]
-
-        # rotated 90 degrees counter-clockwise
-        # expected_4 = [[8, 2, 1, 0],
-        #               [2, 3, 1, 1],
-        #               [6, 4, 1, 5],
-        #               [0, 0, 2, 2]]
-
-        # Rolled clockwise
-        # expected_1 = [[True, True, True, True],
-        #               [True, False, True, False],
-        #               [True, False, False, True],
-        #               [True, False, False, False]]
-        # expected_2 = [[True, True, False, False],
-        #               [True, False, False, False],
-        #               [True, True, False, False],
-        #               [True, True, False, True]]
-        # expected_3 = [[True, False, True, False],
-        #               [True, False, False, False],
-        #               [True, True, False, False],
-        #               [True, False, False, False]]
-        # expected_4 = [[True, False, False, False],
-        #               [True, True, True, False],
-        #               [True, False, False, True],
-        #               [True, True, True, False]]
-
+        # Rolled counter-clockwise
         expected_1 = [[True, True, True, True],
                       [True, False, True, False],
                       [True, False, False, True],
                       [True, False, False, False]]
         expected_2 = [[True, False, False, False],
-                      [True, True, False, False],
-                      [True, False, False, False],
-                      [True, False, True, False]]
-        expected_3 = [[True, True, True, True],
-                      [True, False, True, False],
+                      [True, True, True, False],
                       [True, False, False, True],
-                      [True, False, False, False]]
-        expected_4 = [[True, False, False, False],
-                      [True, True, False, False],
+                      [True, True, True, False]]
+        expected_3 = [[True, False, True, False],
                       [True, False, False, False],
-                      [True, False, True, False]]
+                      [True, True, False, False],
+                      [True, False, False, False]]
+        expected_4 = [[True, True, False, False],
+                      [True, False, False, False],
+                      [True, True, False, False],
+                      [True, True, False, True]]
 
         self.assertEqual(expected_1, actual_1)
-        # self.assertEqual(expected_2, actual_2)
-        # self.assertEqual(expected_3, actual_3)
-        # self.assertEqual(expected_4, actual_4)
+        self.assertEqual(expected_2, actual_2)
+        self.assertEqual(expected_3, actual_3)
+        self.assertEqual(expected_4, actual_4)
 
 
 if __name__ == '__main__':
